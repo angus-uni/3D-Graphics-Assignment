f85ec1a62892870824e44177dfb70f2ae09bd589
Angus <angus.goody@gmail.com>
2022-12-01 13:22:50 +0000

    Wonky looking garden

--- Garden.java
+++ Garden.java
@@ -15,15 +15,21 @@ class Garden {
     private Vec2 cloudPos;
 
     private Texture floorTexture, skyTexture, cloudTexture;
+    private Texture[] textures;
+    private Model[] walls;
+
     private Shader dynamicShader;
     private SGNode roomRoot;
     private Model floorModel, wallModel;
 
 
     private void loadTextures(GL3 gl) {
-        floorTexture = TextureLibrary.loadTexture(gl, "textures/soil.jpg");
-        skyTexture = TextureLibrary.loadTexture(gl, "textures/sky.jpg");
-        cloudTexture = TextureLibrary.loadTexture(gl, "textures/cloud.jpg");
+        textures = new Texture[5];
+        textures[0] = TextureLibrary.loadTexture(gl, "textures/skybox/top.jpg");
+        textures[1] = TextureLibrary.loadTexture(gl, "textures/skybox/front.jpg");
+        textures[2] = TextureLibrary.loadTexture(gl, "textures/skybox/left.jpg");
+        textures[3] = TextureLibrary.loadTexture(gl, "textures/skybox/right.jpg");
+        textures[4] = TextureLibrary.loadTexture(gl, "textures/skybox/bottom.jpg");
     }
 
     public Garden(GL3 gl, Camera c, Light l) {
@@ -36,11 +42,12 @@ class Garden {
         // Setup mesh & shaders
         Mesh mesh = new Mesh(gl, TwoTriangles.vertices.clone(), TwoTriangles.indices.clone());
         Shader shader = new Shader(gl, "shaders/tt_vs.glsl", "shaders/tt_fs.glsl");
+        Material wallMaterial = new Material(new Vec3(0.76f, 0.62f, 0.51f), new Vec3(0.84f,  0.71f,  0.59f), new Vec3(0.3f, 0.3f, 0.3f), 1.0f);
         dynamicShader = new Shader(gl, "shaders/dynamic_background_vs.glsl", "shaders/dynamic_background_fs.glsl");
 
         // Build each model
-        floorModel = buildFloor(gl, mesh, shader);
-        wallModel = buildWall(gl, mesh, dynamicShader);
+        walls = new Model[5];
+        buildWalls(gl, mesh, shader, wallMaterial);
 
         // ====================== Create the scene graph for our room =============================
 
@@ -56,14 +63,14 @@ class Garden {
         // Create the floor node
         NameNode floorNode = new NameNode("Floor");
             TransformNode floorTransform = new TransformNode("Floor transform", mStart);
-                ModelNode floorShape = new ModelNode("floor shape", floorModel);
+                ModelNode floorShape = new ModelNode("floor shape", walls[4]);
 
         // Create the back wall node
         NameNode windowNode = new NameNode("Window");
             Mat4 m = Mat4.multiply(Mat4Transform.rotateAroundX(90), mStart);
             m = Mat4.multiply(Mat4Transform.translate(0,wallSize*0.5f,-wallSize*0.5f), m);
                 TransformNode windowTransform = new TransformNode("Window transform", m);
-                    ModelNode windowShape = new ModelNode("Window shape", wallModel);
+                    ModelNode windowShape = new ModelNode("Window shape", walls[1]);
 
         // Create the left wall node
         NameNode leftWall = new NameNode("Left wall");
@@ -71,7 +78,7 @@ class Garden {
             m = Mat4.multiply(Mat4Transform.rotateAroundZ(-90), m);
             m = Mat4.multiply(Mat4Transform.translate(-wallSize*0.5f,wallSize*0.5f,0), m);
                 TransformNode leftWallTransform = new TransformNode("Left wall transform", m);
-                    ModelNode leftWallShape = new ModelNode("left wall shape", wallModel);
+                    ModelNode leftWallShape = new ModelNode("left wall shape", walls[2]);
 
         // Create the right wall node
         NameNode rightWall = new NameNode("Right wall");
@@ -79,7 +86,7 @@ class Garden {
             m = Mat4.multiply(Mat4Transform.rotateAroundZ(90), m);
             m = Mat4.multiply(Mat4Transform.translate(wallSize*0.5f,wallSize*0.5f,0), m);
                 TransformNode rightWallTransform = new TransformNode("Right wall transform", m);
-                    ModelNode rightWallShape = new ModelNode("Right wall shape", wallModel);
+                    ModelNode rightWallShape = new ModelNode("Right wall shape", walls[3]);
 
 
         // Create Hierarchy
@@ -101,20 +108,18 @@ class Garden {
 
     }
 
-    private Model buildFloor(GL3 gl, Mesh mesh ,Shader shader){
-        Material floorMaterial = new Material(new Vec3(0.76f, 0.62f, 0.51f), new Vec3(0.84f,  0.71f,  0.59f), new Vec3(0.3f, 0.3f, 0.3f), 1.0f);
-        return new Model(gl, camera, light, shader, floorMaterial, new Mat4(), mesh, floorTexture);
+    private void buildWalls(GL3 gl, Mesh mesh, Shader shader, Material material){
+        // Loop through each texture
+        for (int i = 0; i < textures.length; i++) {
+            walls[i] = new Model(gl,camera, light, shader, material, new Mat4(),mesh,textures[i]);
+        }
     }
-
-    private Model buildWall(GL3 gl, Mesh mesh ,Shader shader){
-        Material wallMaterial = new Material(new Vec3(0.76f, 0.62f, 0.51f), new Vec3(0.84f,  0.71f,  0.59f), new Vec3(0.3f, 0.3f, 0.3f), 1.0f);
-        return new Model(gl, camera, light, shader, wallMaterial, new Mat4(), mesh, skyTexture, cloudTexture);
-    }
-
 
     public void render(GL3 gl) {
-        dynamicShader.use(gl);
-        dynamicShader.setFloat(gl, "offset", cloudPos.x, cloudPos.y);
+
+//        dynamicShader.use(gl);
+//        dynamicShader.setFloat(gl, "offset", cloudPos.x, cloudPos.y);
+
         roomRoot.draw(gl);
     }
 
@@ -124,7 +129,8 @@ class Garden {
 
 
     public void dispose(GL3 gl) {
-        floorModel.dispose(gl);
-        wallModel.dispose(gl);
+        for (int i = 0; i < walls.length; i++) {
+            walls[i].dispose(gl);
+        }
     }
 }
